be009072f6c14b44aee944923e8cffaab4761ffe
Gutri Rahmad Zuwa <gutri363696@gmail.com>
2022-10-30 21:33:37 +0700

    Modul13_Gutri Rahmad Zuwa

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Title from "./Title";
+import Body from "./Body";
+import Footer from "./Footer";
+
+function App() {
+  return (
+     <div className="App">
+        <Title />
+        <Body />
+        <Footer />
+     </div>
+  );
+}
+
+export default App;
---
+++ src/Title.js
@@ -0,0 +1,12 @@
+const Title = () => {
+
+  return (
+    <header style={{
+        color: 'white', textAlign: 'center', backgroundColor: '#81B622', fontSize: '20px', padding: '16px', boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', marginBottom: '50px'
+    }}>
+        <h1>Netlab Bookstore</h1>
+    </header>
+  )
+}
+
+export default Title
